a28eaeb825e118cf8acdd3f03b5539dcb157ad71
Hải Long <longdh2.dev@gmail.com>
2025-08-20 20:16:28 +0700

    refactor: restructure metro.config.js for improved clarity and organization

--- metro.config.js
+++ metro.config.js
@@ -2,24 +2,21 @@
 const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+module.exports = (() => {
+  const config = getDefaultConfig(__dirname);
 
-// Khai báo entry để tránh lỗi virtual-metro-entry
-config.server = {
-  ...config.server,
-  enableVisualizer: false,
-};
+  const { transformer, resolver } = config;
 
-// Fix SVG
-config.transformer = {
-  ...config.transformer,
-  babelTransformerPath: require.resolve("react-native-svg-transformer"),
-};
-config.resolver = {
-  ...config.resolver,
-  assetExts: config.resolver.assetExts.filter((ext) => ext !== "svg"),
-  sourceExts: [...config.resolver.sourceExts, "svg", "js", "jsx", "ts", "tsx", "json"],
-};
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
+  };
 
-// Xuất config
-module.exports = withNativeWind(config, { input: "./global.css" });
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
+    sourceExts: [...resolver.sourceExts, "svg"],
+  };
+
+  return withNativeWind(config, { input: "./global.css" });
+})();
