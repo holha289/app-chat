f9485105e3ab19764d5ae6eebe731650ae99923e
Hải Long <longdh2.dev@gmail.com>
2025-09-01 23:32:05 +0700

    feat: update environment configuration and remove unused .env.pod file

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 // src/config/index.ts
 import { NODE_ENV, LOCAL_API_URL, PRODUCT_API_URL } from '@env';
-export const ENV = (NODE_ENV ?? 'local') as 'local' | 'prod';
+export const ENV = (NODE_ENV ?? 'local') as 'local' | 'production';
 console.log("🚀 ~ ENV:", ENV)
-export const API_URL = ENV === 'prod' ? PRODUCT_API_URL : LOCAL_API_URL;
+export const API_URL = ENV === 'production' ? PRODUCT_API_URL : LOCAL_API_URL;
