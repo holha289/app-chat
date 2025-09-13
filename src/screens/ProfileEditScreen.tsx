b1b3622b224b25790677e2b459a7b55b14f862a4
Hải Long <longdh2.dev@gmail.com>
2025-09-14 00:48:13 +0700

    refactor: Replace SafeAreaView with ScrollView in ProfileEditScreen for better layout handling

--- src/screens/ProfileEditScreen.tsx
+++ src/screens/ProfileEditScreen.tsx
@@ -7,10 +7,10 @@ import React from "react";
 import {
   View,
   Text,
-  SafeAreaView,
   Image,
   TouchableOpacity,
   Alert,
+  ScrollView,
 } from "react-native";
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useDispatch, useSelector } from "react-redux";
@@ -21,6 +21,7 @@ import { clsx } from "clsx";
 import { CameraOptions, ImageLibraryOptions, ImagePickerResponse, launchCamera, launchImageLibrary } from "react-native-image-picker";
 import UploadService from "@app/services/upload.service";
 import { requestCameraPermissions, requestFilePermissions, requestMediaPermissions } from "@app/core/permissions";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const ProfileScreen = () => {
   const navigation = useNavigation();
@@ -68,7 +69,7 @@ const ProfileScreen = () => {
         if (error) {
           Alert.alert('Lỗi', error);
         } else {
-           handleGoBack();
+          handleGoBack();
         }
         setLoading(false);
       },
@@ -169,81 +170,83 @@ const ProfileScreen = () => {
   };
 
   return (
-    <SafeAreaView className={ProfileClassStyle.containerEdit}>
+    <SafeAreaView className={ProfileClassStyle.containerEdit} edges={['top']}>
       {/* Ảnh đại diện */}
-      <View style={{ position: 'relative', marginBottom: 24, alignItems: 'center' }}>
-        <Image
-          source={{ uri: form?.avatar || "" }}
-          className={ProfileClassStyle.avatar}
-        />
-        <TouchableOpacity
-          style={ProfileStyle.uploadBtn}
-          onPress={() => handleImagePicker()}
-        >
-          <Ionicons name="camera" size={18} color="white" />
-        </TouchableOpacity>
-      </View>
+      <ScrollView className="w-full flex-1">
+        <View style={{ position: 'relative', marginBottom: 24, alignItems: 'center', flex: 1 }}>
+          <Image
+            source={{ uri: form?.avatar || "" }}
+            className={ProfileClassStyle.avatar}
+          />
+          <TouchableOpacity
+            className="absolute bottom-0 right-[35%] bg-gray-600 p-2 rounded-full"
+            onPress={() => handleImagePicker()}
+          >
+            <Ionicons name="camera" size={18} color="white" />
+          </TouchableOpacity>
+        </View>
 
-      {/* Các nút hành động */}
-      <View className={ProfileClassStyle.actionBtn1Edit}>
-        <View className="mb-4">
-          <Input
-            placeholder="Nhập họ và tên"
-            value={form.fullname}
-            onChangeText={(text) => {
-              setForm({ ...form, fullname: text });
-            }}
-            label="Họ và tên"
-            rounded={10}
-          />
+        {/* Các nút hành động */}
+        <View className={ProfileClassStyle.actionBtn1Edit}>
+          <View className="mb-4">
+            <Input
+              placeholder="Nhập họ và tên"
+              value={form.fullname}
+              onChangeText={(text) => {
+                setForm({ ...form, fullname: text });
+              }}
+              label="Họ và tên"
+              rounded={10}
+            />
+          </View>
+          <View className="mb-4">
+            <Input
+              placeholder="Nhập email"
+              value={form.email}
+              onChangeText={(text) => {
+                setForm({ ...form, email: text });
+              }}
+              label="Email"
+              rounded={10}
+            />
+          </View>
+          <View className="mb-4">
+            <Input
+              placeholder="Nhập số điện thoại"
+              value={form.phone}
+              onChangeText={(text) => {
+                setForm({ ...form, phone: text });
+              }}
+              readOnly={true}
+              label="Số điện thoại"
+              rounded={10}
+            />
+          </View>
+          <View className="mb-4">
+            <Select
+              options={[
+                { value: "male", label: "Nam" },
+                { value: "female", label: "Nữ" },
+                { value: "other", label: "Khác" }
+              ]}
+              value={form.gender}
+              onChange={(value) => setForm({ ...form, gender: value })}
+              placeholder="Chọn giới tính"
+              label="Giới tính"
+            />
+          </View>
         </View>
-        <View className="mb-4">
-          <Input
-            placeholder="Nhập email"
-            value={form.email}
-            onChangeText={(text) => {
-              setForm({ ...form, email: text });
-            }}
-            label="Email"
-            rounded={10}
-          />
+        <View className="w-full">
+          <TouchableOpacity
+            className={clsx(classBtn.primary, 'mt-4')}
+            onPress={handleSave}
+          >
+            <Text className={classBtn.primaryText}>
+              Lưu
+            </Text>
+          </TouchableOpacity>
         </View>
-        <View className="mb-4">
-          <Input
-            placeholder="Nhập số điện thoại"
-            value={form.phone}
-            onChangeText={(text) => {
-              setForm({ ...form, phone: text });
-            }}
-            readOnly={true}
-            label="Số điện thoại"
-            rounded={10}
-          />
-        </View>
-        <View className="mb-4">
-          <Select
-            options={[
-              { value: "male", label: "Nam" },
-              { value: "female", label: "Nữ" },
-              { value: "other", label: "Khác" }
-            ]}
-            value={form.gender}
-            onChange={(value) => setForm({ ...form, gender: value })}
-            placeholder="Chọn giới tính"
-            label="Giới tính"
-          />
-        </View>
-      </View>
-      <View className="w-full">
-        <TouchableOpacity
-          className={clsx(classBtn.primary, 'mt-4')}
-          onPress={handleSave}
-        >
-          <Text className={classBtn.primaryText}>
-            Lưu
-          </Text>
-        </TouchableOpacity>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
